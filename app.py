ea86603a1ff1755842e3f69e8b4d8bf40eaa17ea
shivam-codes <80332745+shivam-codes@users.noreply.github.com>
2023-08-13 11:20:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,13 @@ import pyodbc
 import os
 app = Flask(__name__)
 
-server = "103.91.90.200,33002"
-database = "MRTC_V1"
+server = ""
+database = ""
 #driver = "SQL Server Native Client 11.0"
-username = "sa"
-password = "AsdFZx@#213"
+username = ""
+password = ""
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-para = '0703168'
-query = f'SET NOCOUNT ON; EXEC USP_GetAppEnquiry @cnmt = {para}'
-df = pd.read_sql_query(query, cnxn)
-print(df)
-list = df.values.tolist()
-print(list)
-print(type(df))
 @app.route('/<para>', methods=['GET'])
 def get_data(para):
     #parameter = para
